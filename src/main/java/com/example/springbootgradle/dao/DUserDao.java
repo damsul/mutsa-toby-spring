703e100bbc3f799b611324431bb39f9e06a91914
Damsul <gyoung9707@naver.com>
2023-05-24 14:05:05 +0900

    feat: 템플릿 메소드 패턴 적용

---
+++ src/main/java/com/example/springbootgradle/dao/DUserDao.java
@@ -0,0 +1,12 @@
+package com.example.springbootgradle.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao{
+
+    @Override
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        return null;
+    }
+}
--- src/main/java/com/example/springbootgradle/dao/UserDao.java
+++ src/main/java/com/example/springbootgradle/dao/UserDao.java
@@ -10,18 +10,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
-public class UserDao {
+public abstract class UserDao {
 
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
-        Map<String, String> env = getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
+    public abstract Connection getConnection() throws SQLException, ClassNotFoundException;
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
-        return conn;
-    }
     public void add(User user) throws ClassNotFoundException, SQLException {
         Connection conn = getConnection();
 
@@ -56,20 +48,19 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
+        UserDao userDao = new NUserDao();
 //        userDao.add();
 //        userDao.get();
         User user = new User();
-        user.setId("2");
-        user.setName("gh");
+        user.setId("1");
+        user.setName("damsul");
         user.setPassword("1234");
-//        userDao.add(user);
+        userDao.add(user);
 
         User selectedUser = userDao.get("1");
         System.out.println(selectedUser.getId());
         System.out.println(selectedUser.getName());
         System.out.println(selectedUser.getPassword());
-
     }
 
 }
